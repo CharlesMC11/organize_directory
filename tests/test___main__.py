219e987804da29cc5b912f50f9c6f09fe7fcfe66
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-01-05 22:02:29 -0800

    Test CLI

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -6,18 +6,25 @@ import pytest
 
 from file_organizer.__main__ import main
 
+CONF = r"""
+[dir_names]
+programming = Programming
+python = Programming/Python
+
+[ext_to_dir]
+env = programming
+py = python
+"""
+
 
 def test___main__(capsys, monkeypatch, tmp_path):
     monkeypatch.setattr(sys, "argv", ["prog"])
     with pytest.raises(SystemExit):
         main()
-
     assert "the following arguments are required:" in capsys.readouterr().err
 
     conf = tmp_path / "config.ini"
-    conf.write_text(
-        "[dir_names]\npython = Programming/Python\n\n[ext_to_dir]py = python"
-    )
+    conf.write_text(CONF)
 
     test_dir = tmp_path / "test_dir"
     test_dir.mkdir()
@@ -29,4 +36,4 @@ def test___main__(capsys, monkeypatch, tmp_path):
     main()
     assert "" in capsys.readouterr().err
     assert not test_file.exists()
-    assert (test_dir / "Programming/Python" / test_file.name).exists()
+    assert test_dir.joinpath("Programming", "Python", test_file.name).exists()
